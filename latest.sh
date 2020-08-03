ab52d31471dc59026aedb62711fb2b3a7cffa2ac
root <root@duy.f10.vn>
2020-08-03 11:43:50 +0700

    duy commit

--- latest.sh
+++ latest.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Test connection:
+/usr/bin/curl --silent --header "X-Install: vDDoS" https://files.voduy.com/iplog.php
+/usr/bin/curl -s -w 'Testing Connection Response Time for: github.com\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null -L https://raw.githubusercontent.com/duy13/vDDoS-Protection/master/latest.sh
+/usr/bin/curl -s -w 'Testing Connection Response Time for: sourceforge.net\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null https://sourceforge.net/projects/vddos-protection/files/latest.sh/download
+/usr/bin/curl -s -w 'Testing Connection Response Time for: files.voduy.com\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null https://files.voduy.com/vDDoS-Proxy-Protection/latest.sh
+
+
 # Install vDDoS Proxy Protection:
 latest_version=`/usr/bin/curl -L https://raw.githubusercontent.com/duy13/vDDoS-Protection/master/CHANGELOG.txt|grep '*vDDoS-' |awk 'NR==1' |tr -d '*vDDoS-'|tr -d ':'`
 /usr/bin/curl -L https://github.com/duy13/vDDoS-Protection/raw/master/vddos-$latest_version-centos7 -o /usr/bin/vddos
@@ -9,15 +17,17 @@ if [ "$originhash" != "$downloadhash" ]; then
 	echo 'Download vddos-'$latest_version'-centos7 from Github.com failed! Try Downloading from SourceForge.net...'
 	rm -rf /usr/bin/vddos
 	curl -L https://sourceforge.net/projects/vddos-protection/files/vddos-$latest_version-centos7 -o /usr/bin/vddos --silent
+
+	originhash=`curl -L https://sourceforge.net/projects/vddos-protection/files/md5sum.txt --silent | grep "vddos-$latest_version-centos7" |awk 'NR=1 {print $1}'`
+	downloadhash=`md5sum /usr/bin/vddos | awk 'NR=1 {print $1}'`
+	if [ "$originhash" != "$downloadhash" ]; then
+		echo 'Download vddos-'$latest_version'-centos7 from SourceForge.net failed! Try Downloading from Files.voduy.com...'
+		rm -rf /usr/bin/vddos
+		curl -L https://files.voduy.com/vDDoS-Proxy-Protection/vddos-$latest_version-centos7 -o /usr/bin/vddos --silent
+	fi
+
 fi
-originhash=`curl -L https://sourceforge.net/projects/vddos-protection/files/md5sum.txt --silent | grep "vddos-$latest_version-centos7" |awk 'NR=1 {print $1}'`
-downloadhash=`md5sum /usr/bin/vddos | awk 'NR=1 {print $1}'`
-if [ "$originhash" != "$downloadhash" ]; then
-	echo 'Download vddos-'$latest_version'-centos7 from SourceForge.net failed! Try Downloading from Files.voduy.com...'
-	rm -rf /usr/bin/vddos
-	curl -L https://files.voduy.com/vDDoS-Proxy-Protection/vddos-$latest_version-centos7 -o /usr/bin/vddos --silent
-fi
-/usr/bin/curl --silent --header "X-Install: vDDoS" https://files.voduy.com/iplog.php
+
 chmod 700 /usr/bin/vddos
 /usr/bin/vddos help
 /usr/bin/vddos setup
