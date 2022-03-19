3d107d3e477708fc4385ce3702c064ebebfadd25
root <root@cp53.voduy.com>
2022-03-19 18:06:57 +0700

    duy commit

--- latest.sh
+++ latest.sh
@@ -39,7 +39,12 @@ chmod 755 -R */*.sh  >/dev/null 2>&1
 ./install.sh
 
 if [ -f /vddos/vddos ]; then
+
+	curl -L https://github.com/duy13/vDDoS-Layer4-Mapping/raw/master/vddos-layer4-mapping -o /usr/bin/vddos-layer4
+	chmod 700 /usr/bin/vddos-layer4
+
 	echo 'Install vDDoS Proxy Protection Done!'
+
 	/root/.acme.sh/acme.sh --set-default-ca  --server  letsencrypt >/dev/null 2>&1
 	exit 0
 else
